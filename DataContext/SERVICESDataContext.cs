91c4da2cf5d4eddbd069c3f84b985319adafb4ac
TKDzsola <31966446+TKDzsola@users.noreply.github.com>
2026-01-18 21:29:10 +0100

    Last update

--- DataContext/SERVICESDataContext.cs
+++ DataContext/SERVICESDataContext.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Windows;
@@ -12,28 +13,30 @@ namespace Ecoinv.DataContext
 {
     public class SERVICESDataContext : DataContextBase
     {
+        private readonly SERVICESTable alkTable;
+
         public SERVICESDataContext()
         {
             alkTable = new SERVICESTable();
             SERVICESList = alkTable.GetList(FBConnX);
+
+            // ÁFA kulcsok betöltése
+            var vatTable = new VATRATESTable();
+            VatRates = new ObservableCollection<VATRATES>(vatTable.GetList(FBConnX));
         }
 
-        private readonly SERVICESTable alkTable;
+        public ObservableCollection<VATRATES> VatRates { get; }
 
-        #region ... SERVICESList ObservableCollection<SERVICES> property ...
-
+        #region ... SERVICESList ...
         private ObservableCollection<SERVICES> __SERVICESList = new ObservableCollection<SERVICES>();
-
         public ObservableCollection<SERVICES> SERVICESList
         {
             get => __SERVICESList;
             set => SetPropertyValue(nameof(SERVICESList), ref __SERVICESList, value);
         }
-
         #endregion
 
-        #region ... SelectedSERVICES property ...
-
+        #region ... SelectedSERVICES ...
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private SERVICES __selectedSERVICES;
 
@@ -42,13 +45,12 @@ namespace Ecoinv.DataContext
             get => __selectedSERVICES;
             set
             {
-                OnSelectedSERVICESChanging(value);
-                SetPropertyValue(nameof(SelectedSERVICES), ref __selectedSERVICES, value);
-                OnSelectedSERVICESChanged();
+                if (SetPropertyValue(nameof(SelectedSERVICES), ref __selectedSERVICES, value))
+                {
+                    OnSelectedSERVICESChanged();
+                }
             }
         }
-
-        private void OnSelectedSERVICESChanging(SERVICES value) { }
 
         private void OnSelectedSERVICESChanged()
         {
@@ -62,40 +64,97 @@ namespace Ecoinv.DataContext
                 OLDSERVICES.NETPRICE = SelectedSERVICES.NETPRICE;
                 OLDSERVICES.GROSSPRICE = SelectedSERVICES.GROSSPRICE;
                 OLDSERVICES.VATRATE_ID = SelectedSERVICES.VATRATE_ID;
+
+                // UI segédmezők frissítése
+                _selectedVatRate = VatRates.FirstOrDefault(v => v.ID == SelectedSERVICES.VATRATE_ID);
+                OnPropertyChanged(nameof(SelectedVatRate));
+                OnPropertyChanged(nameof(EditNetPrice));
+                OnPropertyChanged(nameof(EditGrossPrice));
+            }
+        }
+        #endregion
+
+        #region ... AUTO SZÁMOLÁS ...
+
+        // Nettó ár (Int konverzióval javítva)
+        public decimal EditNetPrice
+        {
+            get => SelectedSERVICES?.NETPRICE ?? 0;
+            set
+            {
+                if (SelectedSERVICES != null && SelectedSERVICES.NETPRICE != (int)value)
+                {
+                    // JAVÍTÁS: (int) konverzió
+                    SelectedSERVICES.NETPRICE = (int)value;
+                    OnPropertyChanged(nameof(EditNetPrice));
+                    CalculateGross();
+                }
+            }
+        }
+
+        // Bruttó ár (Int konverzióval javítva)
+        public decimal EditGrossPrice
+        {
+            get => SelectedSERVICES?.GROSSPRICE ?? 0;
+            set
+            {
+                if (SelectedSERVICES != null)
+                {
+                    // JAVÍTÁS: (int) konverzió
+                    SelectedSERVICES.GROSSPRICE = (int)value;
+                    OnPropertyChanged(nameof(EditGrossPrice));
+                }
+            }
+        }
+
+        private VATRATES _selectedVatRate;
+        public VATRATES SelectedVatRate
+        {
+            get => _selectedVatRate;
+            set
+            {
+                if (SetPropertyValue(nameof(SelectedVatRate), ref _selectedVatRate, value))
+                {
+                    if (SelectedSERVICES != null && value != null)
+                    {
+                        SelectedSERVICES.VATRATE_ID = value.ID;
+                        CalculateGross();
+                    }
+                }
+            }
+        }
+
+        private void CalculateGross()
+        {
+            if (SelectedSERVICES != null && SelectedVatRate != null)
+            {
+                decimal multiplier = 1 + (SelectedVatRate.RATES / 100m);
+                // JAVÍTÁS: A számolt decimal értéket int-re kényszerítjük
+                EditGrossPrice = (int)Math.Round(EditNetPrice * multiplier);
             }
         }
 
         #endregion
 
-        #region ... OLDSERVICES property ...
-
+        #region ... OLDSERVICES ...
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private SERVICES __oldSERVICES;
-
         public SERVICES OLDSERVICES
         {
             get => __oldSERVICES;
             set => SetPropertyValue(nameof(OLDSERVICES), ref __oldSERVICES, value);
         }
-
         #endregion
 
-        // =================================================================
-        // JOGOSULTSÁG KEZELÉS (Módosít / Mégse)
-        // =================================================================
-        #region ... CommandModifyCancel property ...
-
+        // --- COMMANDOK ---
+        #region ... CommandModifyCancel ...
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private ICommand __commandModifyCancel;
-
         public ICommand CommandModifyCancel => __commandModifyCancel ??= new DelegateCommand(ac => ModifyCancelExecute(), fc => ModifyCancelCanExecute());
 
         private bool ModifyCancelCanExecute()
         {
-            // Ha szerkesztünk (Cancel funkció), akkor engedjük (hogy ki lehessen lépni)
             if (IsEditing) return true;
-
-            // Ha nem szerkesztünk (Modify funkció), akkor CSAK ADMIN
             return (SelectedSERVICES != null) && DataContextBase.IsAdmin;
         }
 
@@ -107,65 +166,35 @@ namespace Ecoinv.DataContext
                 {
                     alkTable.DelNewservices_M();
                     var _actrec = SERVICESList.FirstOrDefault(r => r.ID == -1);
-                    if (SERVICESList.IndexOf(_actrec) != -1)
-                        SERVICESList.Remove(_actrec);
-
+                    if (SERVICESList.IndexOf(_actrec) != -1) SERVICESList.Remove(_actrec);
                     IsNewRecord = false;
                 }
                 else
                 {
-                    if (SelectedSERVICES != null)
-                        alkTable.ReUpdateservices_M(OLDSERVICES);
+                    if (SelectedSERVICES != null) alkTable.ReUpdateservices_M(OLDSERVICES);
                 }
             }
-
             IsEditing = !IsEditing;
             ShowDetailPanel();
+            OnSelectedSERVICESChanged();
         }
-
         #endregion
 
-        // =================================================================
-        // JOGOSULTSÁG KEZELÉS (Új / Mentés)
-        // =================================================================
-        #region ... CommandNewSave property ...
-
+        #region ... CommandNewSave ...
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private ICommand __commandNewSave;
-
         public ICommand CommandNewSave => __commandNewSave ??= new DelegateCommand(ac => NewSaveExecute(), fc => NewSaveCanExecute());
 
         private bool NewSaveCanExecute()
         {
             if (IsEditing)
             {
-                // MENTÉS MÓD: Validáció + Admin jog ellenőrzése (biztonságból)
-                bool isValid = false;
-                if (IsNewRecord)
-                {
-                    isValid = !string.IsNullOrEmpty(SelectedSERVICES.NAME) &&
-                              !string.IsNullOrEmpty(SelectedSERVICES.DESCRIPTION) &&
-                              !string.IsNullOrEmpty(SelectedSERVICES.NETPRICE.ToString()) &&
-                              !string.IsNullOrEmpty(SelectedSERVICES.GROSSPRICE.ToString()) &&
-                              !string.IsNullOrEmpty(SelectedSERVICES.VATRATE_ID.ToString());
-                }
-                else
-                {
-                    isValid = (SelectedSERVICES != null) &&
-                              !string.IsNullOrEmpty(SelectedSERVICES.NAME) &&
-                              !string.IsNullOrEmpty(SelectedSERVICES.DESCRIPTION) &&
-                              !string.IsNullOrEmpty(SelectedSERVICES.NETPRICE.ToString()) &&
-                              !string.IsNullOrEmpty(SelectedSERVICES.GROSSPRICE.ToString()) &&
-                              !string.IsNullOrEmpty(SelectedSERVICES.VATRATE_ID.ToString());
-                }
-
+                bool isValid = (SelectedSERVICES != null) &&
+                               !string.IsNullOrEmpty(SelectedSERVICES.NAME) &&
+                               SelectedSERVICES.VATRATE_ID > 0;
                 return isValid && DataContextBase.IsAdmin;
             }
-            else
-            {
-                // ÚJ REKORD MÓD: Csak Admin
-                return DataContextBase.IsAdmin;
-            }
+            return DataContextBase.IsAdmin;
         }
 
         private void NewSaveExecute()
@@ -175,51 +204,42 @@ namespace Ecoinv.DataContext
             {
                 IsNewRecord = true;
                 SelectedSERVICES = alkTable.Newservices_M();
+                _selectedVatRate = null;
+                OnPropertyChanged(nameof(SelectedVatRate));
+                OnPropertyChanged(nameof(EditNetPrice));
+                OnPropertyChanged(nameof(EditGrossPrice));
             }
             else
             {
                 if (IsNewRecord)
                 {
-                    if (SelectedSERVICES != null)
-                        alkTable.Insert(SelectedSERVICES, FBConnX);
+                    if (SelectedSERVICES != null) alkTable.Insert(SelectedSERVICES, FBConnX);
                     IsNewRecord = false;
                 }
                 else
                 {
-                    if (SelectedSERVICES != null)
-                        alkTable.Update(SelectedSERVICES, FBConnX);
+                    if (SelectedSERVICES != null) alkTable.Update(SelectedSERVICES, FBConnX);
                 }
             }
             ShowDetailPanel();
         }
-
         #endregion
 
-        // =================================================================
-        // JOGOSULTSÁG KEZELÉS (Törlés)
-        // =================================================================
-        #region ... CommandDelete property ...
-
+        #region ... CommandDelete ...
         [DebuggerBrowsable(DebuggerBrowsableState.Never)]
         private ICommand __commandDelete;
-
         public ICommand CommandDelete => __commandDelete ??= new DelegateCommand(ac => DeleteExecute(), fc => DeleteCanExecute());
 
-        // CSAK ADMIN TÖRÖLHET
-        private bool DeleteCanExecute() => DataContextBase.IsAdmin && (SelectedSERVICES != null) && (!IsEditing);
+        private bool DeleteCanExecute() => (IsAdmin) && (SelectedSERVICES != null) && (!IsEditing);
 
         private void DeleteExecute()
         {
-            MessageBoxResult dres = MessageBox.Show("Biztos a törlésben?", "Törlés", MessageBoxButton.YesNo);
-            if (MessageBoxResult.Yes == dres)
+            if (MessageBox.Show("Biztos a törlésben?", "Törlés", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
             {
-                if (SelectedSERVICES != null)
-                    alkTable.Delete(SelectedSERVICES.ID, FBConnX);
-
+                if (SelectedSERVICES != null) alkTable.Delete(SelectedSERVICES.ID, FBConnX);
                 ShowDetailPanel();
             }
         }
-
         #endregion
     }
 }
